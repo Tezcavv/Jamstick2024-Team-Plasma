1bb665ddc16cade2f249e65e0168d85b04ce726e
Gittorio <vittoriovernassa@gmail.com>
2024-08-08 00:27:32 +0200

    Movimento Implementato !!!

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -1,23 +1,88 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.Windows;
 
 public class PlayerMovement : MonoBehaviour
 {
     public PlayerParams playerParams;
-    public Rigidbody rb;
+    public CharacterController Controller;
+    [Tooltip("How fast the character turns to face movement direction")]
+    [Range(0.0f, 0.3f)]
+    public float RotationSmoothTime = 0.12f;
     private InputAction moveAction;
+    private float _speed;
+    private float _targetRotation = 0.0f;
+    private float _rotationVelocity;
+    private float _verticalVelocity;
+    private float _terminalVelocity = 53.0f;
+    private GameObject _mainCamera;
 
     private void Awake() {
         
         moveAction = GameManager.Instance.inputActions.Player.Move;
+        _mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
-    private void FixedUpdate() {
-        Vector2 movementVector = moveAction.ReadValue<Vector2>().normalized;
-        rb.linearVelocity = playerParams.movementSpeed * new Vector3 (movementVector.x,rb.linearVelocity.y,movementVector.y);
-        transform.LookAt(transform.position + new Vector3(movementVector.x, 0, movementVector.y));
+    private void Update() {
+        Move();
     }
+    private void Move() {
+        float targetSpeed = playerParams.movementSpeed;
+
+        Vector2 movementVector = moveAction.ReadValue<Vector2>();
+
+        // note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+        // if there is no input, set the target speed to 0
+        if (movementVector == Vector2.zero) {
+            targetSpeed = 0.0f;
+        }
+        // a reference to the players current horizontal velocity
+        float currentHorizontalSpeed = new Vector3(Controller.velocity.x, 0.0f, Controller.velocity.z).magnitude;
+
+        float speedOffset = 0.1f;
+
+        // accelerate or decelerate to target speed
+        if (currentHorizontalSpeed < targetSpeed - speedOffset ||
+            currentHorizontalSpeed > targetSpeed + speedOffset) {
+            // creates curved result rather than a linear one giving a more organic speed change
+            // note T in Lerp is clamped, so we don't need to clamp our speed
+            _speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed ,
+                Time.deltaTime * targetSpeed);
+
+            // round speed to 3 decimal places
+            _speed = Mathf.Round(_speed * 1000f) / 1000f;
+        }
+        else {
+            _speed = targetSpeed;
+        }
+
+        // normalise input direction
+        Vector3 inputDirection = new Vector3(movementVector.x, 0.0f, movementVector.y).normalized;
+
+        // note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+        // if there is a move input rotate player when the player is moving
+        if (movementVector != Vector2.zero) {
+            _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg +
+                              _mainCamera.transform.eulerAngles.y;
+            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity,
+                RotationSmoothTime);
+
+            // rotate to face input direction relative to camera position
+            transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+        }
+
+
+        Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
+
+        // move the player
+        Controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) +
+                         new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+
+    }
+
+
 
 
 }
