a129e81f64316fb8df97dde2f6493bbccb436be8
Gittorio <vittoriovernassa@gmail.com>
2024-08-11 15:39:37 +0200

    Virus Spawner non collide più, Globulo Bianco torna indietro

--- Assets/_Scripts/GameLogic/GlobuloBiancoMovement.cs
+++ Assets/_Scripts/GameLogic/GlobuloBiancoMovement.cs
@@ -11,11 +11,13 @@ public class GlobuloBiancoMovement : MonoBehaviour {
     private Rigidbody rb;
     private PlayerMovement player;
     public SphereCollider attackRange;
-    //TODO meglio avere una classe marker
+    //TODO meglio avere una classe marker 
+    Vector3 startPos;
 
     private void Awake() {
         player = FindAnyObjectByType<PlayerMovement>();
         rb = GetComponent<Rigidbody>();
+        startPos = transform.position;
     }
 
     private void FixedUpdate() {
@@ -24,12 +26,20 @@ public class GlobuloBiancoMovement : MonoBehaviour {
 
         GameObject toFollow;
         toFollow = result.FirstOrDefault(c => CheckGameObject(c))?.gameObject;
-        if (toFollow == null) return;
+        if (toFollow == null) {
+            ReturnToStartPoint();
+            return;
+        }
 
         Vector3 movement = (toFollow.transform.position - rb.transform.position).normalized;
         rb.linearVelocity = new Vector3(movement.x, rb.linearVelocity.y, movement.z) * gParams.movementSpeed;
         rb.DOLookAt(toFollow.transform.position, Time.fixedDeltaTime);
 
+    }
+
+    private void ReturnToStartPoint() {
+        Vector3 movement = (startPos- rb.transform.position).normalized;
+        rb.linearVelocity = new Vector3(movement.x, rb.linearVelocity.y, movement.z) * gParams.movementSpeed;
     }
 
     private bool CheckGameObject(Collider c) {
